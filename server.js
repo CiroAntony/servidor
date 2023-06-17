5b6af8144045694e04281b124e8518f6b1cc6aaf
CiroAntony <vasquezciro654@gmail.com>
2023-06-17 15:43:36 -0500

    sprint4

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const upload = multer({ storage: storage });
 // Ruta para el manejo de sesiones
 app.post("/api/login", (req, res) => {
   const { usuario, password } = req.body;
-  const query = `SELECT * FROM usuario WHERE usuario = '${usuario}' AND password = '${password}'`;
+  const query = `SELECT * FROM usuario WHERE usuario = '${usuario}' AND password = '${password}' AND estado = 1`; // Agregar condición "estado = 1"
 
   connection.query(query, (err, results) => {
     if (err) {
@@ -56,7 +56,7 @@ app.post("/api/login", (req, res) => {
       return;
     }
     if (results.length === 0) {
-      res.status(401).send("Credenciales incorrectas");
+      res.status(401).send("Credenciales incorrectas o usuario inactivo"); // Actualizar mensaje de error
       return;
     }
     const user = results[0];
@@ -87,7 +87,7 @@ app.get("/api/protected", requireLogin, (req, res) => {
 
 // Ruta para obtener todas las empresas
 app.get("/api/companies", (req, res) => {
-  const query = "SELECT nombre, ruc, direccion, telefono FROM empresa";
+  const query = "SELECT * FROM empresa";
 
   connection.query(query, (err, results) => {
     if (err) {
@@ -100,19 +100,103 @@ app.get("/api/companies", (req, res) => {
   });
 });
 
-// Ruta para crear una nueva empresa
-app.post("/api/companies", (req, res) => {
-  const { nombre, ruc, direccion, telefono } = req.body;
-  const query = `INSERT INTO empresa (nombre, ruc, direccion, telefono) VALUES ('${nombre}', '${ruc}', '${direccion}', '${telefono}')`;
+// Ruta para obtener una sola empresa
+app.get("/api/companies/:id", (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT id_empresa, nombre, ruc, direccion, telefono, imagen FROM empresa WHERE id_empresa = ${id}`;
 
   connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error al obtener la empresa:", err);
+      res.status(500).json({ error: "Error en el servidor" });
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).json({ error: "No se encontró la empresa" });
+      return;
+    }
+
+    res.json(results[0]);
+  });
+});
+
+// Ruta para crear una nueva empresa con imagen
+app.post("/api/companies", upload.single("imagen"), (req, res) => {
+  const { nombre, ruc, direccion, telefono } = req.body;
+  const imagenPath = req.file.path;
+
+  const imagenData = fs.readFileSync(imagenPath);
+
+  const query = `INSERT INTO empresa (nombre, ruc, direccion, telefono, imagen) VALUES (?, ?, ?, ?, ?)`;
+  const values = [nombre, ruc, direccion, telefono, imagenData];
+
+  connection.query(query, values, (err, results) => {
     if (err) {
       console.error("Error al crear la empresa:", err);
       res.status(500).json({ error: "Error en el servidor" });
       return;
     }
 
+    fs.unlinkSync(imagenPath);
+
     res.status(201).json({ message: "Empresa creada exitosamente" });
+  });
+});
+
+// Ruta para actualizar los datos de una empresa existente con imagen
+app.put("/api/companies/:id", upload.single("imagen"), (req, res) => {
+  const { id } = req.params;
+  const { nombre, ruc, direccion, telefono } = req.body;
+  const imagenPath = req.file?.path;
+
+  let query = "UPDATE empresa SET ";
+  const values = [];
+
+  if (nombre) {
+    query += "nombre = ?, ";
+    values.push(nombre);
+  }
+  if (ruc) {
+    query += "ruc = ?, ";
+    values.push(ruc);
+  }
+  if (direccion) {
+    query += "direccion = ?, ";
+    values.push(direccion);
+  }
+  if (telefono) {
+    query += "telefono = ?, ";
+    values.push(telefono);
+  }
+  if (imagenPath) {
+    const imagenData = fs.readFileSync(imagenPath);
+    query += "imagen = ?, ";
+    values.push(imagenData);
+  }
+
+  // Remove the trailing comma and space
+  query = query.slice(0, -2);
+
+  query += ` WHERE id_empresa = ${id}`;
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error("Error al actualizar la empresa:", err);
+      res.status(500).json({ error: "Error en el servidor" });
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: "No se encontró la empresa" });
+      return;
+    }
+
+    if (imagenPath) {
+      fs.unlinkSync(imagenPath);
+    }
+
+    res.json({ message: "Empresa actualizada exitosamente" });
   });
 });
 
@@ -133,13 +217,22 @@ app.get("/api/usuario", (req, res) => {
 
 // Ruta para crear un nuevo empleado con imagen
 app.post("/api/usuario", upload.single("imagen"), (req, res) => {
-  const { usuario, password, nombres, apellidos, correo, telefono, id_rol } =
-    req.body;
+  const {
+    usuario,
+    password,
+    nombres,
+    apellidos,
+    correo,
+    telefono,
+    id_rol,
+    id_empresa,
+    estado,
+  } = req.body;
   const imagenPath = req.file.path;
 
   const imagenData = fs.readFileSync(imagenPath);
 
-  const query = `INSERT INTO usuario (usuario, password, nombres, apellidos, correo, telefono, id_rol, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const query = `INSERT INTO usuario (usuario, password, nombres, apellidos, correo, telefono, id_rol, id_empresa, estado, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   const values = [
     usuario,
     password,
@@ -148,6 +241,8 @@ app.post("/api/usuario", upload.single("imagen"), (req, res) => {
     correo,
     telefono,
     id_rol,
+    id_empresa,
+    estado,
     imagenData,
   ];
 
@@ -161,6 +256,86 @@ app.post("/api/usuario", upload.single("imagen"), (req, res) => {
     fs.unlinkSync(imagenPath);
 
     res.status(201).json({ message: "Empleado creado exitosamente" });
+  });
+});
+
+// Ruta para actualizar los datos de un empleado existente
+app.put("/api/usuario/:id", upload.single("imagen"), (req, res) => {
+  const { id } = req.params;
+  const {
+    usuario,
+    password,
+    nombres,
+    apellidos,
+    correo,
+    telefono,
+    id_rol,
+    id_empresa,
+    estado,
+  } = req.body;
+  const imagenPath = req.file?.path;
+
+  let query = "UPDATE usuario SET ";
+  const values = [];
+
+  if (usuario) {
+    query += "usuario = ?, ";
+    values.push(usuario);
+  }
+  if (password) {
+    query += "password = ?, ";
+    values.push(password);
+  }
+  if (nombres) {
+    query += "nombres = ?, ";
+    values.push(nombres);
+  }
+  if (apellidos) {
+    query += "apellidos = ?, ";
+    values.push(apellidos);
+  }
+  if (correo) {
+    query += "correo = ?, ";
+    values.push(correo);
+  }
+  if (telefono) {
+    query += "telefono = ?, ";
+    values.push(telefono);
+  }
+  if (id_rol) {
+    query += "id_rol = ?, ";
+    values.push(id_rol);
+  }
+  if (id_empresa) {
+    query += "id_empresa = ?, ";
+    values.push(id_empresa);
+  }
+  if (estado) {
+    query += "estado = ?, ";
+    values.push(estado);
+  }
+  if (imagenPath) {
+    const imagenData = fs.readFileSync(imagenPath);
+    query += "imagen = ?, ";
+    values.push(imagenData);
+  }
+
+  // Eliminar la coma y el espacio adicionales al final de la consulta
+  query = query.slice(0, -2);
+  query += ` WHERE id_usuario = ${id}`;
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error("Error al actualizar el empleado:", err);
+      res.status(500).json({ error: "Error en el servidor" });
+      return;
+    }
+
+    if (imagenPath) {
+      fs.unlinkSync(imagenPath);
+    }
+
+    res.status(200).json({ message: "Empleado actualizado exitosamente" });
   });
 });
 
@@ -183,6 +358,32 @@ app.get("/api/roles", (req, res) => {
 app.get("/api/usuario/:id/imagen", (req, res) => {
   const { id } = req.params;
   const query = `SELECT imagen FROM usuario WHERE id_usuario = ${id}`;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error al obtener la imagen:", err);
+      res.status(500).json({ error: "Error en el servidor" });
+      return;
+    }
+
+    if (results.length === 0 || !results[0].imagen) {
+      res.status(404).json({ error: "No se encontró la imagen" });
+      return;
+    }
+
+    const image = results[0].imagen;
+    res.writeHead(200, {
+      "Content-Type": "image/jpeg",
+      "Content-Length": image.length,
+    });
+    res.end(image);
+  });
+});
+
+// Ruta para obtener la imagen de un empleado por su ID
+app.get("/api/companies/:id/imagen", (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT imagen FROM empresa WHERE id_empresa = ${id}`;
 
   connection.query(query, (err, results) => {
     if (err) {
